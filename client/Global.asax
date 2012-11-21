4f833f9e9a5a27d806a7ff42bb8133b46b901da7
Hekar <hekark@gmail.com>
2012-11-20 20:46:12 -0500

    first commit. added server

---
+++ client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientSite.Global" Language="C#" %>
---
+++ server/Controllers/Default1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace server.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public string Index(string[] values)
+        {
+            return String.Join(",", values);
+        }
+
+    }
+}
